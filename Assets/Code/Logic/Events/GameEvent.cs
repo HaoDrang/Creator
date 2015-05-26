7a70077e6765ed29a35c8589f6523ba77ce31035
HaoDrang <sherlockchang@163.com>
2015-05-26 17:57:09 +0800

    Achievement 01

---
+++ Assets/Code/Logic/Events/GameEvent.cs
@@ -0,0 +1,31 @@
+
+public enum GameEvent
+{
+	GameStart,
+	GameEnd,
+	GamePause,
+
+	StartBeginner,
+	StartEasy,
+	StartNormal,
+	StartHard,
+	StartCrazy,
+	StartOverlord,
+	StartDemigod,
+
+	EndBeginner,
+	EndEasy,
+	EndNormal,
+	EndHard,
+	EndCrazy,
+	EndOverlord,
+	EndDemigod,
+
+	BallShoot,
+	BallCollide,
+	BallConsume,
+	BallDrop,
+
+	RoundBegin,
+	RoundEnd,
+}
--- Assets/Code/Logic/PlayLogic/Achievement/SingleAchievement.cs
+++ Assets/Code/Logic/PlayLogic/Achievement/SingleAchievement.cs
@@ -4,14 +4,16 @@ public class SingleAchievement
 	protected int		miID 			= 0;
 	protected string 	msDisplayName 	= "";
 	protected string 	msKeyName 		= "";
-	protected string[] 	msaConditions 	= null;
+	protected int[] 	miaEvents 	= null;
+	protected int[]		miaEventsMax = null;
 
 	protected const string MainKey = "achievement";
 	protected const string KeyNameKey = "keyname";
 	protected const string DisplayNameKey = "displayname";
-	protected const string ConditionsKey = "conditions";
+	protected const string EventsKey = "event";
+	protected const string EventsMaxKey = "eventmax";
 
-	virtual public void LoadBaseAchievement(int id, AchievementDetection adetection, Property prop)
+	virtual public void Init(int id, AchievementDetection adetection, Property prop)
 	{
 		miID = id;
 
@@ -24,8 +26,38 @@ public class SingleAchievement
 		tempKey = key + "." + DisplayNameKey;
 		msDisplayName = prop.GetString (tempKey);
 		// load condition list
-		tempKey = key + "." + ConditionsKey;
-		msaConditions = prop.GetStringArray (tempKey);
+		tempKey = key + "." + EventsKey;
+		miaEvents = prop.GetIntArray (tempKey);
+		// load event max count
+		tempKey = key + "." + EventsMaxKey;
+		miaEventsMax = prop.GetIntArray (tempKey);
+
+		// register events to change save data
+		RegisterEventHandlers (adetection);
+	}
+
+	virtual public void RegisterEventHandlers(AchievementDetection adt)
+	{
+//		adt.RegisterEvent ();
+	}
+
+	virtual public bool EventHandler(GameEvent eEventType, object[] args)
+	{
+		return true;
+	}
+
+	virtual public void ClearAll()
+	{
+
+	}
+
+	virtual public void ClearRound()
+	{
+
+	}
+
+	virtual public void ClearGame()
+	{
 
 	}
 }
