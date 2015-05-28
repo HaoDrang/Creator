5730117d23cac1f07b4d9757b414ea8d88415a19
HaoDrang <sherlockchang@163.com>
2015-05-28 17:10:30 +0800

    begin build background system

---
+++ Assets/Code/Logic/SceneBackground/ColorSetter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ColorSetter : MonoBehaviour 
+{
+	protected Image target = null;
+
+	void Awake()
+	{
+		target = GetComponent<Image> ();
+	}
+
+	virtual public void SetColor(LevelConfig conf)
+	{
+		if (conf == null) {
+			return;
+		}
+		if (target != null) {
+			target.color = conf.mColors [Random.Range (0, conf.mColors.Length)];
+		} else {
+			Debug.LogWarning("This Game Object Do not have an image component :" + gameObject.name);
+		}
+	}
+}
---
+++ Assets/Code/Test/ColorSetterTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorSetterTest : MonoBehaviour {
+	public ColorSetter cs = null;
+	// Use this for initialization
+	void Start () {
+		cs.SetColor (null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
