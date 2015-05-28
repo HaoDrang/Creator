b638966ca14c3999cd151cd57938990a64ef9fae
HaoDrang <sherlockchang@163.com>
2015-05-28 13:57:42 +0800

    Add a fps watcher
    test if the performance need to be optimized

---
+++ Assets/Code/FrameWork/UI/FPSWatcher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FPSWatcher : MonoBehaviour 
+{
+	private Text t = null;
+	void Awake()
+	{
+		t = GetComponent<Text> ();
+	}
+
+	void Update()
+	{
+		t.text = "FPS:" + (1f / Time.deltaTime);
+	}
+}
