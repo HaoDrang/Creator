877f612d6191a14943129fcca53e1da55d47ef3e
HaoDrang <sherlockchang@163.com>
2015-06-09 15:37:43 +0800

    Update
    remove the build errors

--- Assets/Code/Logic/Ball/Appearance/BallAppearance.cs
+++ Assets/Code/Logic/Ball/Appearance/BallAppearance.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine.UI;
+using UnityEngine;
+using System;
 
 namespace Game.Logic
 {
@@ -9,8 +11,17 @@ namespace Game.Logic
 		protected Image[] Imgs {
 			get { 
 				if (_imgs == null) {
-					_imgs = new Image[(int)BALr.LayerNum];
+					_imgs = new Image[(int)BLR.LayerNum];
 				}
+				return _imgs;
+			}
+		}
+
+		public BallAppearance(Image[] images)
+		{
+			if (images != null) {
+				int copyLength = images.Length < (int)BLR.LayerNum ? images.Length : (int)BLR.LayerNum;
+				Array.Copy(images, Imgs, copyLength);
 			}
 		}
 
@@ -18,26 +29,43 @@ namespace Game.Logic
 		                      Image ShadowLayer = null, Image VeinLayer = null,
 		                      Image AnmLayer = null, Image FrontLayer = null)
 		{
-			SetLayer (BALr.Front, FrontLayer);
-			SetLayer (BALr.Highlight, HighlightLayer);
-			SetLayer (BALr.Shadow, ShadowLayer);
-			SetLayer (BALr.Vein, VeinLayer);
-			SetLayer (BALr.SpecialAnimation, AnmLayer);
-			SetLayer (BALr.BaseColor, BaseLayer);
+			SetLayer (BLR.Front, FrontLayer);
+			SetLayer (BLR.Highlight, HighlightLayer);
+			SetLayer (BLR.Shadow, ShadowLayer);
+			SetLayer (BLR.Vein, VeinLayer);
+			SetLayer (BLR.SpecialAnimation, AnmLayer);
+			SetLayer (BLR.BaseColor, BaseLayer);
 		}
 
-		protected void SetLayer (BALr eLayer, Image layerEntity)
+		protected void SetLayer (BLR eLayer, Image layerEntity)
 		{
 			Imgs [(int)eLayer] = layerEntity;
 		}
 
-		public BallAppearance Set ()
+		public BallAppearance Decorate (BLR eLayer, Sprite sp)
 		{
+			if (Imgs [(int)eLayer] != null) {
+				Imgs [(int)eLayer].sprite = sp;
+			}
 
+			return this;
+		}
+
+		public BallAppearance DecorateWithAnimation(BLR eLayer, string animationName)
+		{
+			if (Imgs [(int)eLayer] != null) {
+				Animator anm = Imgs [(int)eLayer].GetComponent<Animator> ();
+				if (anm != null) 
+				{
+					anm.Play (animationName);
+				}
+			}
+
+			return this;
 		}
 	}
 
-	public enum BALr//Ball Appreance Layer
+	public enum BLR//Ball Appreance Layer
 	{
 		Front,
 		Highlight,
--- Assets/Code/Logic/Ball/Ball.cs
+++ Assets/Code/Logic/Ball/Ball.cs
@@ -1,10 +1,19 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 namespace Game.Logic
 {
 	public class Ball : MonoBehaviour, IRowHandler
 	{
 		protected BallState mState = default(BallState);
+		protected BallAppearance _appearance = null;
+
+		protected Image[] _ballimgs = new Image[(int)BLR.LayerNum];
+
+		virtual public void Awake()
+		{
+
+		}
 
 		virtual public void DetachRow()
 		{
